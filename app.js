d7bbf707dd90c954412fd37ddfb678c3545e66a6
mohammadzohaib23 <mohammadzohaib2398@gmail.com>
2023-09-26 16:38:36 +0500

    latest commit

---
+++ app.js
@@ -0,0 +1,60 @@
+// alert("js");
+
+// document.write("Welcome to Vanilla JS");
+
+// console.log("Welcome to Vanilla JS")
+
+// var number = 50;
+
+// var number2 = 100;
+
+// console.log(number)
+
+// alert(number2)
+
+
+// var firstname = "Muhammad";
+
+// var lastname = "faraz";
+
+// console.log(firstname)
+
+// console.log(lastname)
+
+
+// var number;
+
+// number = 10
+
+// console.log(number)
+
+// var num = 10;
+
+// var result = num + 15;
+
+// console.log(result)
+
+
+// var firstName = "Muhammad";
+
+// var lastname = "irfan";
+
+// console.log(firstName + " " + lastname)
+
+
+var num = 50;
+
+var res = num + 100;
+
+console.log(res)
+
+
+
+
+
+
+
+
+
+
+
